49ef15cc062281c2de51665768acd23ff9f1bbe5
ricciisa <62075153+ricciisa@users.noreply.github.com>
2020-05-21 15:17:03 -0300

    Update BTree_Autoral.c

--- BTree_Autoral.c
+++ BTree_Autoral.c
@@ -3,10 +3,10 @@
 #include <string.h>
 
 #define ORDEM 3
-
+//recursividade tem que ter uma condição para parar?
 //como fazer os testes?
 
-typedef struct{
+typedef struct{       //valores dentro de cada chave
 	long long int val;
 	char nome[65];
 	char email[65];
@@ -15,14 +15,14 @@ typedef struct{
 
 typedef struct{
 	key * keys[ORDEM + 1]; //+1 PARA SPLIT CHILD
-	int count; 
+	int count; //quantidade de chaves dentro do no
   	struct node * filhos[ORDEM + 1];
 }node;
 
 node *createnode(){
-	node * newnode;
-	newnode = (node*)malloc(sizeof(node));
-	newnode->count = 0;
+	node * newnode;   
+	newnode = (node*)malloc(sizeof(node));//aloca memoria para no criado
+	newnode->count = 0; //inicializa contador de chaves
 	/*
 	int i;
 	for (i = 0; i <= ORDEM; i++){
@@ -32,17 +32,17 @@ node *createnode(){
 	return newnode;
 }
 
-node * root = NULL;
+node * root = NULL; //inicializa raiz da arvore
 
 int main(){
 	char c;
 	int cont;
-	long long int contador_global = 1;
-	key * pkey;
-	pkey = (key*)malloc(sizeof(key));
+	long long int contador_global = 1; //valor int da chave
+	key * pkey; // ponteiro para as chaves ->nome ->email ->telefone
+	pkey = (key*)malloc(sizeof(key)); // aloca ponteiro
 	
 	FILE *fptr;
-	fptr = fopen("a.txt", "r");
+	fptr = fopen("a.txt", "r"); //abre arquivo com data set
 	
 	c = getc(fptr);
 	while(c != EOF){
@@ -53,7 +53,7 @@ int main(){
 			c = getc(fptr);
 			cont = 0;
 			while (c!='+'){	
-				pkey->nome[cont] = c;
+				pkey->nome[cont] = c; //pega caracter por caracter e coloca no ->nome
 				cont++;
 				c = getc(fptr);
 			}
@@ -62,7 +62,7 @@ int main(){
 			c = getc(fptr);
 			cont = 0;
 			while (c!='-'){
-				pkey->email[cont] = c;	
+				pkey->email[cont] = c;	//pega caracter por caracter e coloca no ->email
 				cont++;
 				c = getc(fptr);
 			}
@@ -71,7 +71,7 @@ int main(){
 			c = getc(fptr);
 			cont = 0;
 			while (c != '\n' && c != EOF){
-				pkey->telefone[cont] = c;
+				pkey->telefone[cont] = c;//pega caracter por caracter e coloca no ->telefone
 				cont++;	
 				c = getc(fptr);
 			}
@@ -79,7 +79,7 @@ int main(){
 			
 		}
 		printf("%d-%s-%s-%s\n", pkey->val, pkey->nome, pkey->email, pkey->telefone);
-		inserir(pkey, root);
+		inserir(pkey, root); // manda para inserir 
 		
 		contador_global++;
 		
@@ -92,7 +92,7 @@ int main(){
 void inserir(key * pkey, node * root){
 	node * newnode;
 	
-	if (!root){
+	if (!root){ // se for raiz
 		newnode = createnode();
 		atribui_valores(pkey,newnode);
 		newnode->keys[0]->val = pkey->val;
