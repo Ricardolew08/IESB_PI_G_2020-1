2997ff11e058f91916dcb1119fd1f70aede8ca35
Ricardolew08 <ricardo.lewkowicz@gmail.com>
2020-05-31 14:43:46 -0300

    Update BTree_Autoral.c

--- BTree_Autoral.c
+++ BTree_Autoral.c
@@ -2,27 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define ORDEM 3
-//recursividade tem que ter uma condição para parar?
-//como fazer os testes?
+#define ORDEM 3 //máximo de 2 valores por nó
 
-typedef struct{       //valores dentro de cada chave
-	long long int val;
-	char nome[65];
+//FREES APÓS MALLOC
+
+//FAZER TEMPO POR GRÁFICO -> EXCEL
+
+//como fazer os testes? -> USAR BIBLIOTECA COM PRECISÃO DE MICROSEGUNDOS OU RODAR O PROGRAMA 1000X E DIVIDIR POR 1000 O TEMPO.
+
+typedef struct key{
+	long long int val; //INCLUIR ALGUM OUTRO VALOR COMO CHAVE, SE REPETIDO -> NEGAR
+	char nome[65];  //CRIAR CREATEKEY
 	char email[65];
 	char telefone[17];
 }key;
 
-typedef struct{
-	key * keys[ORDEM + 1]; //+1 PARA SPLIT CHILD
-	int count; //quantidade de chaves dentro do no
-  	struct node * filhos[ORDEM + 1];
+typedef struct node{
+	key * keys[ORDEM]; //+1 PARA SPLIT CHILD
+	int count; 
+  	struct node * filhos[ORDEM + 1]; //+1 POR DEFINIÇÃO E +1 PARA SPLIT CHILD
+  	struct node * pai;
 }node;
 
 node *createnode(){
-	node * newnode;   
-	newnode = (node*)malloc(sizeof(node));//aloca memoria para no criado
-	newnode->count = 0; //inicializa contador de chaves
+	node * newnode;
+	newnode = (node*)malloc(sizeof(node));
+	newnode->count = 0;
+	newnode->pai = NULL;
 	/*
 	int i;
 	for (i = 0; i <= ORDEM; i++){
@@ -32,17 +38,17 @@ node *createnode(){
 	return newnode;
 }
 
-node * root = NULL; //inicializa raiz da arvore
+node * root = NULL;
 
 int main(){
 	char c;
 	int cont;
-	long long int contador_global = 1; //valor int da chave
-	key * pkey; // ponteiro para as chaves ->nome ->email ->telefone
-	pkey = (key*)malloc(sizeof(key)); // aloca ponteiro
+	long long int contador_global = 1;
+	key * pkey;
+	pkey = (key*)malloc(sizeof(key));
 	
 	FILE *fptr;
-	fptr = fopen("a.txt", "r"); //abre arquivo com data set
+	fptr = fopen("a.txt", "r");
 	
 	c = getc(fptr);
 	while(c != EOF){
@@ -53,7 +59,7 @@ int main(){
 			c = getc(fptr);
 			cont = 0;
 			while (c!='+'){	
-				pkey->nome[cont] = c; //pega caracter por caracter e coloca no ->nome
+				pkey->nome[cont] = c;
 				cont++;
 				c = getc(fptr);
 			}
@@ -62,7 +68,7 @@ int main(){
 			c = getc(fptr);
 			cont = 0;
 			while (c!='-'){
-				pkey->email[cont] = c;	//pega caracter por caracter e coloca no ->email
+				pkey->email[cont] = c;	
 				cont++;
 				c = getc(fptr);
 			}
@@ -71,7 +77,7 @@ int main(){
 			c = getc(fptr);
 			cont = 0;
 			while (c != '\n' && c != EOF){
-				pkey->telefone[cont] = c;//pega caracter por caracter e coloca no ->telefone
+				pkey->telefone[cont] = c;
 				cont++;	
 				c = getc(fptr);
 			}
@@ -79,7 +85,7 @@ int main(){
 			
 		}
 		printf("%d-%s-%s-%s\n", pkey->val, pkey->nome, pkey->email, pkey->telefone);
-		inserir(pkey, root); // manda para inserir 
+		inserir(pkey, root);
 		
 		contador_global++;
 		
@@ -91,46 +97,157 @@ int main(){
 
 void inserir(key * pkey, node * root){
 	node * newnode;
+	int flag;
 	
-	if (!root){ // se for raiz
+	if (!root){
 		newnode = createnode();
 		atribui_valores(pkey,newnode);
 		newnode->keys[0]->val = pkey->val;
 		root = newnode;
+	}else{
+		flag = find_add(pkey,root,0);
+		if(!flag){ //arrumar isso
+			printf ("Valor inserido");
+		}
 	}
-	
-	find_node(pkey,root);
-	
 }
 
-void find_node(key * pkey, node * mynode){
-	int flag;
-	if(pkey->valor < mynode->keys[0]->valor){
-		find_node()
+int find_add(key * pkey, node * mynode, int pospai){
+	int flag, pos;
+	
+	if (!mynode){
+		return 1;
+	}
+	
+	for (pos = 0; pos < mynode->count; pos++){
+		if (pkey->val < mynode->keys[pos]->val){	
+			break;
+		}
+	}
+	
+	flag = find_add(pkey,mynode->filhos[pos], pospai);
+	
+	if (flag){	
+		atribui_valores(pkey, mynode);
+	}
+	
+	if(mynode->count == ORDEM){
+		split_child(mynode, pospai);
+	}
+	return 0;
+}
+
+int split_child(node * mynode, int pospai){
+	node * newnode;
+	newnode = createnode();
+	int i, j = 0, mediana = ORDEM/2;
+	
+	for (i = 0; i < ORDEM; i++){ //POPULAR NOVO NÓ
+		if (i > mediana){ 
+			newnode->keys[j] = mynode->keys[i];
+			newnode->count++;
+			mynode->count--; //DAR FREE NA CHAVE RETIRADA
+			j++;
+		}
+	}
+	
+	if(mynode->pai){	
+		
+		atribuir_valor(mynode->keys[mediana], mynode->pai);
+		newnode->pai = mynode->pai;
+	
+		for (i = mynode->pai->count; i > pospai ; i--){ //i > posição que mediana assumiu no nó pai
+			if (mynode->pai->filhos[i]){
+				mynode->pai->filhos[i+1] = mynode->pai->filhos[i];
+			}
+		}
+		
+		newnode->pai->filhos[pospai+1] = newnode;
+		
+		j= 0;
+		
+		for (i = mediana+1; i < ORDEM; i++){ //ajustar filhos de novos nós
+			newnode->filhos[j] = mynode->filhos[i];
+			mynode->filhos[i] = NULL; //ou free?
+			j++;
+		}
+		
+	}else{
+		
+		node * newroot;
+		newroot = createnode();
+		
+		atribuir_valor(mynode->keys[mediana], newroot);
+		root = newroot;
+		
+		mynode->pai = newroot;
+		
+		newroot->filhos[0] = mynode;
+		newroot->filhos[1] = newnode;
 	}
 }
 
 void atribui_valores(key * pkey, node * mynode){
 	int i, pos;
 	
-	for (i = 0; i <= ORDEM; i++){
-		if (pkey->val < mynode->keys[i]->val){
+	for (i = mynode->count-1; i >= 0; i--){
+		if (mynode->keys[i]->val < pkey->val){
 			pos = i;
 			break;
 		}
+		mynode->keys[i+1] = mynode->keys[i]; //MOVENDO CHAVES PARA POSIÇÕES MAIS AVANÇADAS DO NÓ
 	}
-	mynode->keys[i+1] = mynode->keys[i];
 	
-	mynode->keys[i] = pkey;
+	mynode->keys[i+1] = pkey; //*pkey
 	mynode->count++;
+}
+
+int find_remove(long long int val, node *mynode){
 	
-	if (mynode->count == ORDEM+1){
-		//splitchild(mynode);
+	
+	if (!mynode){
+		return 1;
+	}
+	
+	for (pos = 0; pos < mynode->count; pos++){ //se der merda, olhar o pos
+	
+		if (val == mynode->keys[pos]->val){
+			remover_valores(val, mynode, pos);
+			return 0;
+		}
+		
+		if (val < mynode->keys[pos]->val){	
+			break;
+		}
+	}
+	
+	flag = find_remove(val,mynode->filhos[pos]);
+
+	if (flag){
+		return 1;
+	}
+
+	return 0;
+}
+
+void remover_valores (long long int val, node * mynode, int pos){
+	int i;
+	for (i = pos; i < mynode->count; i++){
+		mynode->keys[pos]=mynode->keys[pos+1];
+	}
+	mynode->count--;
+	
+	if(mynode->link[0]){
+		
+	}
+	
+	if(mynode->count < ORDEM/2){
+		
 	}
 }
 
 
-	//printf("%d %d %s %s %s", i, mynode->keys[i]->val, mynode->keys[i]->nome, mynode->keys[i]->email, mynode->keys[i]->telefone);
+//printf("%d %d %s %s %s", i, mynode->keys[i]->val, mynode->keys[i]->nome, mynode->keys[i]->email, mynode->keys[i]->telefone);
 
 
 
