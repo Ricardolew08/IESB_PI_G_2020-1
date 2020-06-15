6fb0dfac7cf2b6b7884da3428e1995c7325afe60
Ricardolew08 <ricardo.lewkowicz@gmail.com>
2020-06-15 17:12:20 -0300

    Update BTree_Autoral.c

--- BTree/BTree_Autoral.c
+++ BTree/BTree_Autoral.c
@@ -2,261 +2,517 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define ORDEM 3 //máximo de 2 valores por nó
+#define MIN 3
 
-//FREES APÓS MALLOC
+typedef struct key
+{
+    int val;
+    char name[64], email[64];
+} key;
 
-//FAZER TEMPO POR GRÁFICO -> EXCEL
+typedef struct node
+{
+    key *keys[((2 * MIN) - 1)];
+    int count, leaf;
+    struct node *child[2 * MIN];
+} node;
 
-//como fazer os testes? -> USAR BIBLIOTECA COM PRECISÃO DE MICROSEGUNDOS OU RODAR O PROGRAMA 1000X E DIVIDIR POR 1000 O TEMPO.
+node *ROOT = NULL;
+node *createnode(int isleaf);
+void find_insert(node *mynode, key *newkey);
+void splitchild(node *newroot, int i, node *mynode);
+void insert(key *newkey);
+void populate(node *mynode, key *newkey, int i);
+void printar(node *mynode);
+key *getant(node *mynode, int i);
+key *getsuc(node *mynode, int i);
+void deleteleaf(node *mynode, int i);
+void deletenotleaf(node *mynode, int i);
+void borrant(node *mynode, int i);
+void borrsuc(node *mynode, int i);
+void merge(node *mynode, int i);
+void fill(node *mynode, int i);
+void find_delete(node *mynode, int data);
+void delete (int data);
 
-typedef struct key{
-	long long int val; //INCLUIR ALGUM OUTRO VALOR COMO CHAVE, SE REPETIDO -> NEGAR
-	char nome[65];  //CRIAR CREATEKEY
-	char email[65];
-	char telefone[17];
-}key;
+int main()
+{
+    int stringIndex, key;
+    char fileChar, strToInt[10], name[64], email[64];
 
-typedef struct node{
-	key * keys[ORDEM]; //+1 PARA SPLIT CHILD
-	int count; 
-  	struct node * filhos[ORDEM + 1]; //+1 POR DEFINIÇÃO E +1 PARA SPLIT CHILD
-  	struct node * pai;
-}node;
 
-node *createnode(){
-	node * newnode;
-	newnode = (node*)malloc(sizeof(node));
-	newnode->count = 0;
-	newnode->pai = NULL;
-	/*
+    int arr[2000000], qtd = 0;
+
+    FILE *fptr;
+    fptr = fopen("DatasetPI-10k-Tele9Digitos.txt", "r");
+
+    struct key *newkey;
+    newkey = (struct key *)malloc(sizeof(struct key));
+
+    fileChar = fgetc(fptr);
+    while (fileChar != EOF)
+    {
+
+        if (fileChar == '*')
+        {
+            fileChar = fgetc(fptr);
+            stringIndex = 0;
+            while (fileChar != '+')
+            {
+                newkey->name[stringIndex++] = fileChar;
+                fileChar = fgetc(fptr);
+            }
+            newkey->name[stringIndex] = '\0';
+
+            fileChar = fgetc(fptr);
+            stringIndex = 0;
+            while (fileChar != '-')
+            {
+                newkey->email[stringIndex++] = fileChar;
+                fileChar = fgetc(fptr);
+            }
+            newkey->email[stringIndex] = '\0';
+
+            fileChar = fgetc(fptr);
+            stringIndex = 0;
+            while (fileChar != '\n' && fileChar != EOF)
+            {
+                strToInt[stringIndex++] = fileChar;
+                fileChar = fgetc(fptr);
+            }
+            strToInt[stringIndex] = '\0';
+            newkey->val = atoi(strToInt);
+        }
+        arr[qtd++] = newkey->val;
+
+        insert(newkey);
+
+        fileChar = fgetc(fptr);
+    }
+    
+    printar(ROOT);
+    
 	int i;
-	for (i = 0; i <= ORDEM; i++){
-		newnode->keys[i] = (key*)malloc(sizeof(key)); 
+	for (i=0;i<qtd;i++){
+		printf("Deletando %d\n",arr[i]);
+        delete(arr[i]);
 	}
-	*/
-	return newnode;
+    printar(ROOT);
+    return 0;
 }
 
-node * root = NULL;
-
-int main(){
-	char c;
-	int cont;
-	long long int contador_global = 1;
-	key * pkey;
-	pkey = (key*)malloc(sizeof(key));
-	
-	FILE *fptr;
-	fptr = fopen("a.txt", "r");
-	
-	c = getc(fptr);
-	while(c != EOF){
-		
-		pkey->val = contador_global;
-		
-		if (c == '*'){
-			c = getc(fptr);
-			cont = 0;
-			while (c!='+'){	
-				pkey->nome[cont] = c;
-				cont++;
-				c = getc(fptr);
-			}
-			pkey->nome[cont] = '\0';
-			
-			c = getc(fptr);
-			cont = 0;
-			while (c!='-'){
-				pkey->email[cont] = c;	
-				cont++;
-				c = getc(fptr);
-			}
-			pkey->email[cont] = '\0';
-			
-			c = getc(fptr);
-			cont = 0;
-			while (c != '\n' && c != EOF){
-				pkey->telefone[cont] = c;
-				cont++;	
-				c = getc(fptr);
-			}
-			pkey->telefone[cont] = '\0';
-			
-		}
-		printf("%d-%s-%s-%s\n", pkey->val, pkey->nome, pkey->email, pkey->telefone);
-		inserir(pkey, root);
-		
-		contador_global++;
-		
-		c = getc(fptr);
-	}
-	return 0;
-	//printf("%s %s %s", no.nome, no.email, no.telefone);
+node *createnode(int isleaf)
+{
+    node *newnode = (node *)malloc(sizeof(node));
+    newnode->leaf = isleaf;
+    newnode->count = 0;
+    int i;
+    for (i = 0; i < 2 * MIN - 1; i++)
+    {
+        newnode->keys[i] = NULL;
+    }
+    for (i = 0; i < 2 * MIN; i++)
+    {
+        newnode->child[i] = NULL;
+    }
+    return newnode;
 }
 
-void inserir(key * pkey, node * root){
-	node * newnode;
-	int flag;
-	
-	if (!root){
-		newnode = createnode();
-		atribui_valores(pkey,newnode);
-		newnode->keys[0]->val = pkey->val;
-		root = newnode;
-	}else{
-		flag = find_add(pkey,root,0);
-		if(!flag){ //arrumar isso
-			printf ("Valor inserido");
-		}
-	}
+void find_insert(node *mynode, key *newkey)
+{
+    int i = mynode->count - 1;
+
+    if (mynode->leaf)
+    {
+        while (i >= 0 && mynode->keys[i]->val > newkey->val)
+        {
+            mynode->keys[i + 1] = mynode->keys[i];
+            i--;
+        }
+        populate(mynode, newkey, i + 1);
+    }
+    else
+    {
+        while (i >= 0 && mynode->keys[i]->val > newkey->val)
+        {
+            i--;
+        }
+        if (mynode->child[i + 1]->count == ((2 * MIN) - 1))
+        {
+            splitchild(mynode, i + 1, mynode->child[i + 1]);
+            if (mynode->keys[i + 1]->val < newkey->val)
+            {
+                i++;
+            }
+        }
+        find_insert(mynode->child[i + 1], newkey);
+    }
 }
 
-int find_add(key * pkey, node * mynode, int pospai){
-	int flag, pos;
-	
-	if (!mynode){
-		return 1;
-	}
-	
-	for (pos = 0; pos < mynode->count; pos++){
-		if (pkey->val < mynode->keys[pos]->val){	
-			break;
-		}
-	}
-	
-	flag = find_add(pkey,mynode->filhos[pos], pospai);
-	
-	if (flag){	
-		atribui_valores(pkey, mynode);
-	}
-	
-	if(mynode->count == ORDEM){
-		split_child(mynode, pospai);
-	}
-	return 0;
+void splitchild(node *newroot, int i, node *mynode)
+{
+    node *newnode = createnode(mynode->leaf);
+    newnode->count = MIN - 1;
+
+    int j;
+    for (j = 0; j < MIN - 1; j++)
+    {
+        //newnode->keys[j] = (key *)malloc(sizeof(key));
+        newnode->keys[j] = mynode->keys[j + MIN];
+        mynode->keys[j + MIN] = NULL;
+    }
+
+    if (!mynode->leaf)
+    {
+        for (j = 0; j < MIN; j++)
+        {
+            //newnode->child[j] = (node *)malloc(sizeof(node));
+            newnode->child[j] = mynode->child[j + MIN];
+            mynode->child[j + MIN] = NULL;
+        }
+    }
+    mynode->count = MIN - 1;
+
+    for (j = newroot->count; j >= i + 1; j--)
+    {
+        newroot->child[j + 1] = newroot->child[j];
+    }
+
+    newroot->child[i + 1] = newnode;
+
+    for (j = newroot->count - 1; j >= i; j--)
+    {
+        newroot->keys[j + 1] = newroot->keys[j];
+    }
+    populate(newroot, mynode->keys[MIN - 1], i);
+    //newroot->keys[i] = mynode->keys[MIN - 1];
+    //newroot->count++;
 }
 
-int split_child(node * mynode, int pospai){
-	node * newnode;
-	newnode = createnode();
-	int i, j = 0, mediana = ORDEM/2;
-	
-	for (i = 0; i < ORDEM; i++){ //POPULAR NOVO NÓ
-		if (i > mediana){ 
-			newnode->keys[j] = mynode->keys[i];
-			newnode->count++;
-			mynode->count--; //DAR FREE NA CHAVE RETIRADA
-			j++;
-		}
-	}
-	
-	if(mynode->pai){	
-		
-		atribuir_valor(mynode->keys[mediana], mynode->pai);
-		newnode->pai = mynode->pai;
-	
-		for (i = mynode->pai->count; i > pospai ; i--){ //i > posição que mediana assumiu no nó pai
-			if (mynode->pai->filhos[i]){
-				mynode->pai->filhos[i+1] = mynode->pai->filhos[i];
-			}
-		}
-		
-		newnode->pai->filhos[pospai+1] = newnode;
-		
-		j= 0;
-		
-		for (i = mediana+1; i < ORDEM; i++){ //ajustar filhos de novos nós
-			newnode->filhos[j] = mynode->filhos[i];
-			mynode->filhos[i] = NULL; //ou free?
-			j++;
-		}
-		
-	}else{
-		
-		node * newroot;
-		newroot = createnode();
-		
-		atribuir_valor(mynode->keys[mediana], newroot);
-		root = newroot;
-		
-		mynode->pai = newroot;
-		
-		newroot->filhos[0] = mynode;
-		newroot->filhos[1] = newnode;
-	}
+void populate(node *mynode, key *mykey, int i)
+{
+
+    mynode->keys[i] = (key *)malloc(sizeof(key));
+
+    mynode->keys[i]->val = mykey->val;
+    strcpy(mynode->keys[i]->name, mykey->name);
+    strcpy(mynode->keys[i]->email, mykey->email);
+
+    mynode->count++;
 }
 
-void atribui_valores(key * pkey, node * mynode){
-	int i, pos;
-	
-	for (i = mynode->count-1; i >= 0; i--){
-		if (mynode->keys[i]->val < pkey->val){
-			pos = i;
-			break;
-		}
-		mynode->keys[i+1] = mynode->keys[i]; //MOVENDO CHAVES PARA POSIÇÕES MAIS AVANÇADAS DO NÓ
-	}
-	
-	mynode->keys[i+1] = pkey; //*pkey
-	mynode->count++;
+void insert(key *newkey)
+{
+    if (!ROOT)
+    {
+        ROOT = createnode(1);
+        populate(ROOT, newkey, 0);
+    }
+    else
+    {
+        if (ROOT->count == (((2 * MIN) - 1)))
+        {
+            node *newroot = createnode(0);
+            newroot->child[0] = ROOT;
+            splitchild(newroot, 0, ROOT);
+            int i = 0;
+            if (newroot->keys[0]->val < newkey->val)
+            {
+                i++;
+            }
+
+            find_insert(newroot->child[i], newkey);
+
+            ROOT = newroot;
+        }
+        else
+        {
+            find_insert(ROOT, newkey);
+        }
+    }
 }
 
-int find_remove(long long int val, node *mynode){
-	
-	
-	if (!mynode){
-		return 1;
-	}
-	
-	for (pos = 0; pos < mynode->count; pos++){ //se der merda, olhar o pos
-	
-		if (val == mynode->keys[pos]->val){
-			remover_valores(val, mynode, pos);
-			return 0;
-		}
-		
-		if (val < mynode->keys[pos]->val){	
-			break;
-		}
-	}
-	
-	flag = find_remove(val,mynode->filhos[pos]);
-
-	if (flag){
-		return 1;
-	}
-
-	return 0;
+void printar(node *mynode)
+{
+    int i;
+    if (!ROOT)
+    {
+        printf("Arvore Vazia.");
+        return;
+    }
+    for (i = 0; i < mynode->count; i++)
+    {
+        if (!mynode->leaf)
+        {
+            printar(mynode->child[i]);
+        }
+        // printf("%d\t%p", i, mynode);
+        printf("%d\t%s\t%s\n", mynode->keys[i]->val, mynode->keys[i]->name, mynode->keys[i]->email);
+    }
+    if (!mynode->leaf)
+    {
+        printar(mynode->child[i]);
+    }
 }
 
-void remover_valores (long long int val, node * mynode, int pos){
-	int i;
-	for (i = pos; i < mynode->count; i++){
-		mynode->keys[pos]=mynode->keys[pos+1];
-	}
-	mynode->count--;
-	
-	if(mynode->link[0]){
-		
-	}
-	
-	if(mynode->count < ORDEM/2){
-		
-	}
+int find(node *mynode, int data)
+{
+    int i = 0;
+    while (i < mynode->count && mynode->keys[i]->val < data)
+    {
+        i++;
+    }
+    return i;
 }
 
+key *getant(node *mynode, int i)
+{
+    node *ptr = mynode->child[i];
+    while (!ptr->leaf)
+    {
+        ptr = ptr->child[ptr->count];
+    }
+    return ptr->keys[ptr->count - 1];
+}
 
-//printf("%d %d %s %s %s", i, mynode->keys[i]->val, mynode->keys[i]->nome, mynode->keys[i]->email, mynode->keys[i]->telefone);
+key *getsuc(node *mynode, int i)
+{
+    node *ptr = mynode->child[i + 1];
+    while (!ptr->leaf)
+    {
+        ptr = ptr->child[0];
+    }
+    return ptr->keys[0];
+}
 
+void deleteleaf(node *mynode, int i)
+{
+    int j;
+    for (j = i + 1; j < mynode->count; ++j)
+    {
+        mynode->keys[j - 1] = mynode->keys[j];
+    }
+    mynode->count--;
+}
 
+void deletenotleaf(node *mynode, int i)
+{
+    int k = mynode->keys[i]->val;
 
+    if (mynode->child[i]->count >= MIN)
+    {
+        key *ant = getant(mynode, i);
+        mynode->keys[i] = ant; //*ant???
+        find_delete(mynode->child[i], ant->val);
+    }
 
+    else if (mynode->child[i + 1]->count >= MIN)
+    {
+        key *suc = getsuc(mynode, i);
+        mynode->keys[i] = suc;
+        find_delete(mynode->child[i+1], suc->val);
+    }
 
+    else
+    {
+        merge(mynode, i);
+        find_delete(mynode->child[i], k);
+    }
+}
 
+void borrant(node *mynode, int i)
+{
+    node *filho = mynode->child[i];
+    node *irm = mynode->child[i - 1];
 
+    int j;
+    for (j = filho->count - 1; j >= 0; --j)
+    {
+        filho->keys[j + 1] = filho->keys[j];
+    }
+    if (!filho->leaf)
+    {
+        for (j = filho->count; j >= 0; --j)
+        {
+            filho->child[j + 1] = filho->child[j];
+        }
+    }
+    filho->keys[0] = mynode->keys[i - 1];
 
+    if (!filho->leaf)
+    {
+        filho->child[0] = irm->child[irm->count];
+    }
 
+    mynode->keys[i - 1] = irm->keys[irm->count - 1];
 
+    filho->count++;
+    irm->count--;
+}
 
+void borrsuc(node *mynode, int i)
+{
+    node *filho = mynode->child[i];
+    node *irm = mynode->child[i + 1];
 
+    filho->keys[filho->count] = mynode->keys[i];
+
+    if (!filho->leaf)
+    {
+        filho->child[filho->count + 1] = irm->child[0];
+    }
+
+    mynode->keys[i] = irm->keys[0];
+
+    int j;
+    for (j = 1; j < irm->count; ++j)
+    {
+        irm->keys[j - 1] = irm->keys[j];
+    }
+
+    if (!irm->leaf)
+    {
+        for (j = 1; j <= irm->count; ++j)
+        {
+            irm->child[j - 1] = irm->child[j];
+        }
+    }
+
+    filho->count++;
+    irm->count--;
+}
+
+void merge(node *mynode, int i)
+{
+    node *filho = mynode->child[i];
+    node *irm = mynode->child[i + 1];
+
+    filho->keys[MIN - 1] = mynode->keys[i];
+
+    int j;
+    for (j = 0; j < irm->count; ++j)
+    {
+        filho->keys[j + MIN] = irm->keys[j];
+    }
+
+    if (!filho->leaf)
+    {
+        for (j = 0; j <= irm->count; ++j)
+        {
+            filho->child[j + MIN] = irm->child[j];
+        }
+    }
+
+    for (j = i + 1; j < mynode->count; ++j)
+    {
+        mynode->keys[j - 1] = mynode->keys[j];
+    }
+
+    for (j = i + 2; j <= mynode->count; ++j)
+    {
+        mynode->child[j - 1] = mynode->child[j];
+    }
+
+    filho->count += irm->count + 1;
+    mynode->count--;
+
+    free (irm);
+}
+
+void fill(node *mynode, int i)
+{
+    if (i != 0 && mynode->child[i - 1]->count >= MIN)
+    {
+        borrant(mynode, i);
+    }
+
+    else if (i != mynode->count && mynode->child[i + 1]->count >= MIN)
+    {
+        borrsuc(mynode, i);
+    }
+
+    else
+    {
+        if (i != mynode->count)
+        {
+            merge(mynode, i);
+        }
+        else
+        {
+            merge(mynode, i - 1);
+        }
+    }
+}
+
+void find_delete(node *mynode, int data)
+{
+    int i = find(mynode, data); 
+
+    if (i < mynode->count && mynode->keys[i]->val == data) //NÃO TÁ ENTRANDO AQUI E DEVERIA, PROVAVEL PROBLEMA NO FIND -> TALVEZ ALGUMA FUNÇÃO ESTEJA FAZENDO O COUNT ERRADO
+    {
+        if (mynode->leaf)
+        {
+            deleteleaf(mynode, i);
+        }
+        else
+        {
+            deletenotleaf(mynode, i);
+        }
+    }
+    else
+    {
+        if (mynode->leaf)
+        {
+            printf("A chave %d nao existe.\n", data);
+            return;
+        }
+
+        int flag;
+        if (i == mynode->count)
+        {
+            flag = 1;
+        }
+        else
+        {
+            flag = 0;
+        }
+
+        if (mynode->child[i]->count < MIN)
+        {
+            fill(mynode, i);
+        }
+
+        if (flag && i > mynode->count)
+        {
+            find_delete(mynode->child[i - 1], data);
+        }
+        else
+        {
+            find_delete(mynode->child[i], data);
+        }
+    }
+}
+
+void delete (int data)
+{
+    if (!ROOT)
+    {
+        printf("Arvore Vazia.\n");
+        return;
+    }
+    find_delete(ROOT, data);
+
+    if (!ROOT->count)
+    {
+        node *ptr = ROOT;
+        if (ROOT->leaf)
+        {
+            ROOT = NULL;
+        }
+        else
+        {
+            ROOT = ROOT->child[0];
+        }
+        free(ptr);
+    }
+}
